8f4f3f80fb667e53e5c4d814ffa1378207f251e2
mrbassx <abassiddrisu9222@gmail.com>
2025-02-14 18:59:19 +0000

    hh

--- Firebase/src/main.cpp
+++ Firebase/src/main.cpp
@@ -9,7 +9,7 @@
 
 /* Put your SSID & Password */
 const char* ssid = "ESP32";  // Enter SSID here
-const char* password = "12345678";  //Enter Password here
+const char* password = "123456789";  //Enter Password here
 
 /* Put IP Address details */
 IPAddress local_ip(192,168,1,1);
@@ -45,8 +45,8 @@ bool LED2status = LOW;
  #define DATABASE_URL "fir-iot-e9926-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
  
  /* 4. Define the user Email and password that alreadey registerd or added in your project */
- #define USER_EMAIL "abassiddrisu9222@gmail.com"
- #define USER_PASSWORD "Sooner419@"
+ #define USER_EMAIL "admin@gmail.com"
+ #define USER_PASSWORD "admin12345@"
  
  // Define Firebase Data object
  FirebaseData fbdo;
@@ -70,11 +70,11 @@ bool LED2status = LOW;
 
    Serial.print("Connecting to Wi-Fi");
    unsigned long ms = millis();
-  //  while (WiFi.status() != WL_CONNECTED)
-  //  {
-  //    Serial.print(".");
-  //    delay(300);
-  //  }
+   while (WiFi.status() != WL_CONNECTED)
+   {
+     Serial.print(".");
+     delay(300);
+   }
    Serial.println();
    Serial.print("Connected with IP: ");
    Serial.println(WiFi.localIP());
@@ -152,93 +152,22 @@ bool LED2status = LOW;
      // For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create_Edit_Parse.ino
      FirebaseJson json;
  
-     if (count == 0)
-     {
-       json.set("value/round/" + String(count), F("cool!"));
-       json.set(F("value/ts/.sv"), F("timestamp"));
-       Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, F("/test/json"), &json) ? "ok" : fbdo.errorReason().c_str());
-     }
-     else
-     {
-       json.add(String(count), F("smart!"));
-       Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, F("/test/json/value/round"), &json) ? "ok" : fbdo.errorReason().c_str());
-     }
+    //  if (count == 0)
+    //  {
+    //    json.set("value/round/" + String(count), F("cool!"));
+    //    json.set(F("value/ts/.sv"), F("timestamp"));
+    //    Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, F("/test/json"), &json) ? "ok" : fbdo.errorReason().c_str());
+    //  }
+    //  else
+    //  {
+    //    json.add(String(count), F("smart!"));
+    //    Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, F("/test/json/value/round"), &json) ? "ok" : fbdo.errorReason().c_str());
+    //  }
  
      Serial.println();
- 
-     // For generic set/get functions.
- 
-     // For generic set, use Firebase.RTDB.set(&fbdo, <path>, <any variable or value>)
- 
-     // For generic get, use Firebase.RTDB.get(&fbdo, <path>).
-     // And check its type with fbdo.dataType() or fbdo.dataTypeEnum() and
-     // cast the value from it e.g. fbdo.to<int>(), fbdo.to<std::string>().
- 
-     // The function, fbdo.dataType() returns types String e.g. string, boolean,
-     // int, float, double, json, array, blob, file and null.
- 
-    
- 
+
      count++;
    }
  }
  
- /** NOTE:
-  * When you trying to get boolean, integer and floating point number using getXXX from string, json
-  * and array that stored on the database, the value will not set (unchanged) in the
-  * FirebaseData object because of the request and data response type are mismatched.
-  *
-  * There is no error reported in this case, until you set this option to true
-  * config.rtdb.data_type_stricted = true;
-  *
-  * In the case of unknown type of data to be retrieved, please use generic get function and cast its value to desired type like this
-  *
-  * Firebase.RTDB.get(&fbdo, "/path/to/node");
-  *
-  * float value = fbdo.to<float>();
-  * String str = fbdo.to<String>();
-  *
-  */
  
- /// PLEASE AVOID THIS ////
- 
- // Please avoid the following inappropriate and inefficient use cases
- /**
-  *
-  * 1. Call get repeatedly inside the loop without the appropriate timing for execution provided e.g. millis() or conditional checking,
-  * where delay should be avoided.
-  *
-  * Everytime get was called, the request header need to be sent to server which its size depends on the authentication method used,
-  * and costs your data usage.
-  *
-  * Please use stream function instead for this use case.
-  *
-  * 2. Using the single FirebaseData object to call different type functions as above example without the appropriate
-  * timing for execution provided in the loop i.e., repeatedly switching call between get and set functions.
-  *
-  * In addition to costs the data usage, the delay will be involved as the session needs to be closed and opened too often
-  * due to the HTTP method (GET, PUT, POST, PATCH and DELETE) was changed in the incoming request.
-  *
-  *
-  * Please reduce the use of swithing calls by store the multiple values to the JSON object and store it once on the database.
-  *
-  * Or calling continuously "set" or "setAsync" functions without "get" called in between, and calling get continuously without set
-  * called in between.
-  *
-  * If you needed to call arbitrary "get" and "set" based on condition or event, use another FirebaseData object to avoid the session
-  * closing and reopening.
-  *
-  * 3. Use of delay or hidden delay or blocking operation to wait for hardware ready in the third party sensor libraries, together with stream functions e.g. Firebase.RTDB.readStream and fbdo.streamAvailable in the loop.
-  *
-  * Please use non-blocking mode of sensor libraries (if available) or use millis instead of delay in your code.
-  *
-  * 4. Blocking the token generation process.
-  *
-  * Let the authentication token generation to run without blocking, the following code MUST BE AVOIDED.
-  *
-  * while (!Firebase.ready()) <---- Don't do this in while loop
-  * {
-  *     delay(1000);
-  * }
-  *
-  */
